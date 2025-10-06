4a7757ea98979829ff39bcac699d71de9fe6d8bd
new Mic <vaibhav8560@gmail.com.com>
2025-10-06 09:50:59 +0530

    Fixed the directory component image url path

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -5,31 +5,31 @@ const categories = [
   {
     id : 1,
     title : 'Proteins',
-    imgUrl : './src/img/Protein.png',
+    imgUrl : 'https://i.ibb.co/CKh78cfQ/Protein.png',
     route : 'shop/proteins'
   },
   {
     id : 2,
     title : 'Vitamins & Supplements',
-    imgUrl : './src/img/Vitamins.jpg',
+    imgUrl : 'https://i.ibb.co/YTF9Y6Gs/Vitamins.jpg',
     route : 'shop/vitamins & supplements'
   },
   {
     id : 3,
     title : 'Skin Care',
-    imgUrl : './src/img/Skin.png',
+    imgUrl : 'https://i.ibb.co/TDT3XN71/Skin.png',
     route : 'shop/skin care'
   },
   {
     id : 4,
     title : "Women's Health",
-    imgUrl : './src/img/Woman.jpg',
+    imgUrl : 'https://i.ibb.co/Zp7cPbNh/close-up-portrait-brunette-fitness-woman-taking-dietary-supplements-vitamins-glass-water-1258-189573.jpg',
     route : "shop/women's health"
   },
   {
     id : 5,
     title : "Men's Health",
-    imgUrl : './src/img/Man.png',
+    imgUrl : 'https://i.ibb.co/k70c2Gt/Man.png',
     route : "shop/men's health"
   }
 ];
